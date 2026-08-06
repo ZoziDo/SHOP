0b6cc2df15c41b9a603fdf59b658baece7793f4a
f0rb <163629061+ZoziDo@users.noreply.github.com>
2026-08-06 12:19:31 +0700

    Update terminal_config.lua

--- terminal_config.lua
+++ terminal_config.lua
@@ -2,7 +2,7 @@
 -- Адреса смотрите командой: lua /home/check_components.lua
 return {
     -- Вставьте адрес МОДЕМА серверного компьютера, не адрес самого компьютера.
-    serverAddress = "ВСТАВЬТЕ_АДРЕС_МОДЕМА_СЕРВЕРА",
+    serverAddress = "20fbc003-9a01-4669-be30-60e5034d45c9",
     port = 1414,
     serverName = "Default",
 
